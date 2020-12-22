f417d433a23bef084303cfe31d2daa94e205a1c3
Mish <mishuct26@gmail.com>
2020-12-22 13:00:55 +0200

    one load commit

---
+++ js/main.js
@@ -0,0 +1,106 @@
+/*
+* Template Name: MyPortfolio
+* Template URL: https://bootstrapmade.com/myportfolio-bootstrap-portfolio-website-template/
+* License: https://bootstrapmade.com/license/
+*/
+
+(function ($) {
+  "use strict";
+
+  var burgerMenu = function() {
+	  $('.burger').click(function(e) {
+	  	$(window).scrollTop(0);
+	    if(!$('.burger').hasClass('active'))
+	      $('.burger').addClass('active');
+	    else
+	      $('.burger').removeClass('active');
+	  });
+  }
+  burgerMenu();
+
+  var siteIstotope = function() {
+	  var $container = $('#portfolio-grid').isotope({
+	    itemSelector : '.item',
+	    isFitWidth: true
+	  });
+
+	  $(window).resize(function(){
+	    $container.isotope({
+	      columnWidth: '.col-sm-3'
+	    });
+	  });
+	  
+	  $container.isotope({ filter: '*' });
+
+	  $('#filters').on( 'click', 'a', function(e) {
+	  	e.preventDefault();
+	    var filterValue = $(this).attr('data-filter');
+	    $container.isotope({ filter: filterValue });
+	    $('#filters a').removeClass('active');
+	    $(this).addClass('active');
+	  });
+  }
+  $(window).on('load', function () {
+    siteIstotope();
+  });
+
+
+  var siteOwlCarousel = function() {
+  	$('.testimonial-carousel').owlCarousel({
+		  center: true,
+	    items: 1,
+	    loop: true,
+	    margin: 0,
+	    autoplay: true,
+	    smartSpeed: 1000,
+		});
+  };
+  siteOwlCarousel();
+
+     // loader
+   var loader = function () {
+	setTimeout(function () {
+		if ($('#loader').length > 0) {
+			$('#loader').removeClass('show');
+		}
+	}, 1);
+};
+loader();
+
+
+})(jQuery);
+
+AOS.init({
+	easing: 'ease',
+	duration: 1000,
+	once: true
+});
+
+//Initiate the wowjs
+new WOW().init();
+
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 200) {
+		$('.back-to-top').fadeIn('slow');
+	} else {
+		$('.back-to-top').fadeOut('slow');
+	}
+});
+$('.back-to-top').click(function () {
+	$('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+	return false;
+});
+
+
+    
+
+    
+
+    
+   
+    
+    
+   
+   
+    
+
